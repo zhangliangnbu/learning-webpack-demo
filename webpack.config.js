fbaec07e9e3c7ad71f69393baf37e0008da6eae6
zhang.liang <zhang.liang@yintech.cn>
2017-12-15 09:27:13 +0800

    add some

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
+
+// const path = require('path');
+//
+// module.exports = {
+//   entry: './src/index.js',
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'dist')
+//   }
+// };
